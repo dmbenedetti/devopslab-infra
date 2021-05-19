e49e642a901c8a1a96c4b83816db8a503dcb6071
Daniel Merlin Benedetti <benedetti@gmail.com>
2021-05-18 22:52:22 -0300

    Remover credenciais do Google

--- providers.tf
+++ providers.tf
@@ -11,8 +11,6 @@ terraform {
 # Configura o Provider Google Cloud com o Projeto
 provider "google" {
 
-  credentials = file("/vagrant/lab-devops-cloud-314122-e13635f3da2e.json")
-
   project = "lab-devops-cloud-314122"
   region  = "us-central1"
   zone    = "us-central1-c"
